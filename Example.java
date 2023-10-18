851e6e5dc69e7c95d17c4b462b430d5a1bce488a
Benjamin Kitson <benjaminkitson13@gmail.com>
2023-10-18 17:13:38 +0100

    data structures

---
+++ Example.java
@@ -0,0 +1,11 @@
+public class Example {
+    private String first;
+
+    public Example(String first) {
+        this.first = first;
+    }
+
+    public void printFirst() {
+        System.out.println(this.first);
+    }
+}
---
+++ stack/Stack.java
@@ -0,0 +1,49 @@
+package stack;
+
+public class Stack {
+    private Node top;
+    private int height;
+
+    public class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public void push(int value) {
+        Node newNode = new Node(value);
+        if (height > 0) {
+            newNode.next = top;
+        }
+        top = newNode;
+        height++;
+    }
+
+    public Node pop() {
+        if (height == 0) {
+            return null;
+        }
+        Node temp = top;
+        top = top.next;
+        temp.next = null;
+        height--;
+        return temp;
+    }
+
+    public Stack(int value) {
+        Node newNode = new Node(value);
+        top = newNode;
+        height = 1;
+    }
+
+    public void printList() {
+        Node temp = top;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+}
